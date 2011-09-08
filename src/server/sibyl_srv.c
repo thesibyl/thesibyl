cd278a9552c07306993e60264cae5f9102b41d9c
Rafa Casado <rafacas@gmail.com>
2011-09-08 15:12:22 +0200

    Added some comments to the new functions of sibyl_srv

--- src/server/sibyl_srv.c
+++ src/server/sibyl_srv.c
@@ -43,6 +43,20 @@ void *get_in_addr(struct sockaddr *sa)
 	return &(((struct sockaddr_in6*)sa)->sin6_addr);
 }
 
+/* read_keys: 
+ * read the private keys
+ *
+ * INPUT:
+ *   **decrypt, **sign -> pointer to the private keys (decrypting/signing)
+ *
+ * OUTPUT:
+ *   SIBYL_SUCCESS if the privates keys could being retrieved
+ *   SIBYL_KEYS_ERROR if any error ocurred
+ * 
+ * COLLATERAL EFFECTS:
+ *   The variables **decrypt and **sign are filled with the appropriate content
+ *
+ */
 int read_keys(RSA **decrypt,
 	      RSA **sign){
 
@@ -51,15 +65,15 @@ int read_keys(RSA **decrypt,
 	decr_fname = (char *)calloc(_POSIX_PATH_MAX, sizeof(char));
 	sign_fname = (char *)calloc(_POSIX_PATH_MAX, sizeof(char));
 	if(decr_fname == NULL || sign_fname == NULL){
-		perror("decr_fname or sign_fname alloc");
-		return(errno);
+		D("Error: decr_fname or sign_fname alloc");
+		return(SIBYL_KEYS_ERROR);
 	}
 
 	if(strlen(SIBYL_DIR) >= _POSIX_PATH_MAX ||
 	   FILE_LEN >= _POSIX_PATH_MAX ||
 	   (strlen(SIBYL_DIR) + 1 + FILE_LEN) >= _POSIX_PATH_MAX){
-		perror("dir length");
-		return(errno);
+		D("Error: dir length");
+		return(SIBYL_KEYS_ERROR);
 	}
 
         snprintf(decr_fname, _POSIX_PATH_MAX, "%s/%s", SIBYL_DIR, SIBYL_DECR_KEY);
@@ -68,22 +82,22 @@ int read_keys(RSA **decrypt,
 	/* Fetch the private keys */
 	FILE *decr_f, *sign_f;
 	if((decr_f = fopen(decr_fname, "r")) == NULL){
-		perror("Unable to open file decr_f");
-		return(errno);
+		D("Error: Unable to open file decr_f");
+		return(SIBYL_KEYS_ERROR);
 	}
 	if((sign_f = fopen(sign_fname, "r")) == NULL){
-		perror("Unable to open file sign_f");
-		return(errno);
+		D("Error: Unable to open file sign_f");
+		return(SIBYL_KEYS_ERROR);
 	}
 
 	/* RSA *decrypt *sign */
 	if((*decrypt = RSA_new()) == NULL){
-		perror("Unable to RSA_new() decrypt");
-		return(errno);
+		D("Error: Unable to RSA_new() decrypt");
+		return(SIBYL_KEYS_ERROR);
 	}
 	if((*sign = RSA_new()) == NULL){
-		perror("Unable to RSA_new() sign");
-		return(errno);
+		D("Error: Unable to RSA_new() sign");
+		return(SIBYL_KEYS_ERROR);
 	}
 
 	/* Read the private keys */
@@ -91,16 +105,16 @@ int read_keys(RSA **decrypt,
 	PEM_read_RSAPrivateKey(sign_f, sign, NULL, NULL);
 
 	if((*decrypt)->n == NULL){
-		perror("Error reading the RSA decrypt key");
+		D("Error reading the RSA decrypt key");
 		fclose(decr_f);
 		fclose(sign_f);
-		return(errno);
+		return(SIBYL_KEYS_ERROR);
 	}
 	if((*sign)->n == NULL){
-		perror("Error reading the RSA sign key");
+		D("Error reading the RSA sign key");
 		fclose(decr_f);
 		fclose(sign_f);
-		return(errno);
+		return(SIBYL_KEYS_ERROR);
 	}
 	fclose(decr_f);
 	fclose(sign_f);
@@ -108,6 +122,20 @@ int read_keys(RSA **decrypt,
 	return(SIBYL_SUCCESS);
 }
 
+/* start_server:
+ *   Create the socket where the sibyl will be listening
+ *
+ * INPUT:
+ *   *sock -> socket where the server will be listening
+ *
+ * OUTPUT:
+ *   SIBYL_SUCCESS if the socket is created sucessfully
+ *   SIBYL_LISTEN_ERROR if any error ocurred
+ *
+ * COLLATERAL EFFECTS:
+ *   The *sock variable is filled with the appropriate content
+ *
+ */
 int start_server(int *sock){
 	struct addrinfo hints, *srvinfo, *p;
 	int status; 
@@ -119,29 +147,28 @@ int start_server(int *sock){
 	hints.ai_socktype = SOCK_STREAM;
 	hints.ai_flags = AI_PASSIVE;
 
-	/* Start listening */
 	if ((status = getaddrinfo(NULL, SIBYL_PORT, &hints, &srvinfo)) != 0) {
-		fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
-		return(-1);
+		D1("Error: getaddrinfo  %s\n", gai_strerror(status));
+		return(SIBYL_LISTEN_ERROR);
 	}
 
 	// loop through all the results and bind to the first we can
 	for(p = srvinfo; p != NULL; p = p->ai_next){
 		if ((*sock = socket(p->ai_family, p->ai_socktype,
 				p->ai_protocol)) == -1) {
-			perror("server: socket");
+			D("Error: server socket");
 			continue;
 		}
 
 		if (setsockopt(*sock, SOL_SOCKET, SO_REUSEADDR, &yes,
 				sizeof(int)) == -1){
-			perror("server: setsockopt");
+			D("Error: server setsockopt");
 			continue;
 		}
 
-  	if (bind(*sock, p->ai_addr, p->ai_addrlen) == -1) {
+	  	if (bind(*sock, p->ai_addr, p->ai_addrlen) == -1) {
 			close((int)*sock);
-			perror("server: bind");
+			D("Error: server bind");
 			continue;
 		}
 
@@ -149,59 +176,115 @@ int start_server(int *sock){
 	}
 
 	if (p == NULL) {
-		fprintf(stderr, "server: failed to bind\n");
-		return(-1);
+		D("Error: server failed to bind");
+		return(SIBYL_LISTEN_ERROR);
 	}
 
 	freeaddrinfo(srvinfo);
 
+	/* Start listening */
 	if (listen(*sock, SIBYL_BACKLOG) == -1) {
-		perror("listen");
-		return(errno);
+		D("Error: listening");
+		return(SIBYL_LISTEN_ERROR);
 	}
 
 	sa.sa_handler = sigchld_handler; // reap all dead processes
 	sigemptyset(&sa.sa_mask);
 	sa.sa_flags = SA_RESTART;
 	if (sigaction(SIGCHLD, &sa, NULL) == -1){
-		perror("sigaction");
-		return(errno);
+		D("Error: sigaction");
+		return(SIBYL_LISTEN_ERROR);
 	}
 
 	return (SIBYL_SUCCESS);
 }
 
-int send_nonce(int *sock,
+/* send_nonce
+ *   Once the client's connection is accepted the server send a random nonce
+ * 
+ * INPUT:
+ *   sock -> socket connected to the client
+ *   *strnonce -> pointer to the string where the nonce will be stored
+ *
+ * OUTPUT:
+ *   SIBYL_SUCCESS if the nonce is generated and sent with no errors
+ *   SIBYL_NONCE_ERROR if any error ocurred
+ *
+ * COLLATERAL EFFECTS:
+ *   The *strnonce is filled with the nonce
+ *
+ */
+int send_nonce(int sock,
 	       char **strnonce){
 	u_char nonce[9];
 	int count;
 
+	// seed some bytes into the PRNG
+	FILE *rand_f;
+	int seed;
+	struct timeval tv;
+	if((rand_f = fopen("/dev/random", "r")) == NULL){
+		gettimeofday(&tv,0);
+		seed = tv.tv_sec + tv.tv_usec;
+	} else {
+		if(fread(&seed,sizeof(seed),1,rand_f) == 1) {
+			fclose(rand_f);
+		} else {
+			gettimeofday(&tv,0);
+			seed = tv.tv_sec + tv.tv_usec;
+		}
+	}
+	RAND_seed((const void *)&seed, sizeof(seed));
+
 	// generate a random nonce.
         // this may need to be larger than 8 bytes
 	if (*strnonce == NULL){
-		perror("strnonce calloc");
-		return(errno);
+		D("Error: strnonce calloc");
+		return(SIBYL_NONCE_ERROR);
 	}
 	RAND_bytes(nonce, 8);
 	for(count = 0; count < 8; count++)
 		sprintf((*strnonce)+count*2, "%02X", nonce[count]);			
+
 	// send the nonce
-	if (send(*sock, *strnonce, 17, 0) == -1){
-		perror("send strnonce");
-		return(errno);
+	if (send(sock, *strnonce, 17, 0) == -1){
+		D("Error: sending strnonce");
+		return(SIBYL_NONCE_ERROR);
 	}
 
 	// the nonce ends in '@'
-	if (send(*sock, "@", 1, 0) == -1){
-		perror("send '@'");
-		return(errno);
+	if (send(sock, "@", 1, 0) == -1){
+		D("Error: sending '@'");
+		return(SIBYL_NONCE_ERROR);
 	}
 
 	return(SIBYL_SUCCESS);
 }
 
+/* receive_msg
+ *   
+ *
+ * INPUT:
+ *   *msg -> message received
+ *   sock -> socket connected to the client
+ *   token[3] -> array where the 3 items parsed from the message will be stored,
+ *   	      they are:
+ *   	      token[0] = m
+ *   	      token[1] = p1
+ *   	      token[2] = p2
+ *
+ * OUTPUT:
+ *   SIBYL_SUCCESS if the message is received and parsed with no errors
+ *   SIBYL_RECV_ERROR if there is any error receiving the message from the client
+ *   SIBYL_NASTY_CLIENT if the client is sending more bytes than necessary
+ *   SIBYL_MALFORMED_MSG if the message received is malformed
+ *
+ * COLLATERAL EFFECTS:
+ *   The token array is filled with the item parsed from the message.
+ *
+ */
 int receive_msg(char **msg,
-		int *sock,
+		int sock,
 		char *token[3]){
 	int count_bytes = 0;
 	int bytes_rcvd = 0;
@@ -212,7 +295,7 @@ int receive_msg(char **msg,
 		 (*((*msg)+(count_bytes-2)) == '@') &&
 		 (*((*msg)+(count_bytes-3)) == '@') &&
 		 (*((*msg)+(count_bytes-4)) == '\n'))){
-		if((bytes_rcvd = recv(*sock, *msg + count_bytes,
+		if((bytes_rcvd = recv(sock, *msg + count_bytes,
 				      SIBYL_MAX_MSG - count_bytes, 0)) <= 0){
 			perror("Connection error with the client.");
 			return(SIBYL_RECV_ERROR);
@@ -241,7 +324,7 @@ int receive_msg(char **msg,
 	/* remove the end-of-message notification */
 	char *new_msg = (char *)calloc(count_bytes-4, sizeof(char));
         if(new_msg == NULL){
-		D("Unable to allocate memory for new_msg");
+		D("Error: Unable to allocate memory for new_msg");
 		return(errno);
         }
 	strncpy(new_msg, *msg, count_bytes-4);
@@ -249,45 +332,61 @@ int receive_msg(char **msg,
 	/* token[0] = m */
 	token[0] = strsep(&new_msg, ";");
 	if(token[0] == NULL){
-		D("Malformed message received from the client");
+		D("Error: Malformed message received from the client");
 		return(SIBYL_MALFORMED_MSG);
 	}
 	/* token[1] = p1 */
 	token[1] = strsep(&new_msg, ";");
 	if(token[1] == NULL){
-		D("Malformed message received from the client");
+		D("Error: Malformed message received from the client");
 		return(SIBYL_MALFORMED_MSG);
 	}
 	/* token[2] = p2 */
 	token[2] = strsep(&new_msg, ";");
 	if(token[2] == NULL){
-		D("Malformed message received from the client");
+		D("Error: Malformed message received from the client");
 		return(SIBYL_MALFORMED_MSG);
 	}
 	/* there should not be more tokens */
 	if(strsep(&new_msg, ";") != NULL){
-		D("Malformed message received from the client");
+		D("Error: Malformed message received from the client");
 		return(SIBYL_MALFORMED_MSG);
 	}
 
 	return(SIBYL_SUCCESS);
 }
 
-int decrypt_token(char *p1_data,
+/* decrypt_token
+ *   decrypt the token (p1 or p2) received and parsed from the client's message
+ *
+ * INPUT:
+ *   *p_data -> pointer to store the decrypted token in
+ *   *token -> string with the token (p1 or p2) 
+ *   *decrypt -> private key for decryption
+ *
+ * OUTPUT:
+ *   SIBYL_SUCCESS if the decryption is successful
+ *   SIBYL_OPENSSL_ERROR if there is any error decrypting the token
+ *
+ * COLLATERAL EFFECTS:
+ *   *p_data is filled with the decrypted token
+ *
+ */
+int decrypt_token(char *p_data,
 	          char *token,
 	          RSA *decrypt){
 	int rsa_d;
-	char *p1_rsa = (char *)calloc(RSA_size(decrypt) + 1, 1);
-	if(p1_rsa == NULL){
-		D("Unable to allocate memory for token_rsa");
+	char *p_rsa = (char *)calloc(RSA_size(decrypt) + 1, 1);
+	if(p_rsa == NULL){
+		D("Error: Unable to allocate memory for token_rsa");
 		return(errno);
 	}
 	b64_pton(token,
-		 (u_char *)p1_rsa,
+		 (u_char *)p_rsa,
 		 RSA_size(decrypt) + 1);
 	rsa_d = RSA_private_decrypt(RSA_size(decrypt),
-				    (u_char *)p1_rsa,
-				    (u_char *)p1_data,
+				    (u_char *)p_rsa,
+				    (u_char *)p_data,
 				    decrypt,
 				    RSA_PKCS1_OAEP_PADDING);
 	if (rsa_d == -1){
@@ -298,6 +397,26 @@ int decrypt_token(char *p1_data,
 	return(SIBYL_SUCCESS);
 }
 
+/* is_pwd_ok
+ *   Check if the password is ok, that is, check if p1_data equals
+ *   to v1 (p2_data = n:v1)
+ *
+ * INPUT:
+ *   *p1_data -> string with the entry in the 'shadow' file corresponding
+ *               to the user
+ *   *p2_data -> n:v1 where n is the nonce generated by the sibyl and v1 is
+ *               crypt(passwd,salt) of the password type by the user
+ *   **auth_result -> '0' is the authentication is OK and '1' otherwise
+ *   *strnonce -> string with the nonce generated by the sibyl
+ *
+ * OUTPUT:
+ *   SIBYL_SUCCESS if there is no problems
+ *   SIBYL_MALFORMED_MSG if the p2_data is malformed
+ *
+ * COLLATERAL EFFECTS:
+ *   *auth_result is filled with the result of the password check
+ *
+ */
 int is_pwd_ok(char *p1_data,
 	      char *p2_data,
 	      char **auth_result,
@@ -308,13 +427,13 @@ int is_pwd_ok(char *p1_data,
 	/* p2_token[0] = nonce */
 	p2_token[0] = strsep(&p2_data, ":");
 	if(p2_token[0] == NULL){
-		D("Malformed p2_data");
+		D("Error: Malformed p2_data");
 		return(SIBYL_MALFORMED_MSG);
 	}
 	/* p2_token[1] = v1 */
 	p2_token[1] = strsep(&p2_data, ":");
 	if(p2_token[1] == NULL){
-		D("Malformed p2_data");
+		D("Error: Malformed p2_data");
 		return(SIBYL_MALFORMED_MSG);
 	}
 
@@ -336,10 +455,25 @@ int is_pwd_ok(char *p1_data,
 	return(SIBYL_SUCCESS);
 }
 
+/* send_response
+ *   creates and send the response to the client
+ *
+ * INPUT:
+ *   sock -> socket connected to the client
+ *   token[3] -> array with m; p1 and p2
+ *   auth_result -> char with the result of the authentication
+ *   sign -> private key for signing
+ *
+ * OUTPUT:
+ *   SIBYL_SUCCESS if the response is created and sent with no problems
+ *   SIBYL_SSL_ERROR if there is any problem signing the response
+ *
+ */
 int send_response(int *sock,
 		  char *token[3],
 		  char *auth_result,
 		  RSA *sign){
+
 	/* Create the response, which is as follows:
  	 * M;signature
  	 * where M is a message (text without semicolons)
@@ -356,7 +490,7 @@ int send_response(int *sock,
 	message = (char *) calloc(strlen(token[0]) + 1 + 
 				  strlen(auth_result), sizeof(char));
 	if (message == NULL){
-		D("Unable to allocate memory for message");
+		D("Error: Unable to allocate memory for message");
 		return(errno);
 	}
 	// TODO: use strncat instead of strcat with a NONCE_LENGHT const
@@ -369,7 +503,7 @@ int send_response(int *sock,
 	/* computes the SHA-1 message digest (20 bytes) */
 	char *sha1_m = (char *) calloc(20, sizeof(char));
 	if (sha1_m == NULL){
-		D("Unable to allocate memory for sha1_m");
+		D("Error: Unable to allocate memory for sha1_m");
 		return(errno);
 	}
 	SHA1((u_char *)message, strlen(message), (u_char*)sha1_m); 
@@ -377,7 +511,7 @@ int send_response(int *sock,
 	/* sign the message digest */
 	char *signature = (char *) calloc(RSA_size(sign) + 1, 1);
 	if (signature == NULL){
-		D("Unable to allocate memory for signature");
+		D("Error: Unable to allocate memory for signature");
 		return(errno);
 	}
 	u_int siglen;
@@ -395,7 +529,7 @@ int send_response(int *sock,
 	/* encode the signature to base-64 */
 	char *signature_b64 = (char *)calloc(RSA_size(sign) * 4,1);
 	if(signature_b64 == NULL){
-		D("Unable to allocate memory for signature_b64");
+		D("Error: Unable to allocate memory for signature_b64");
 		return(errno);
 	}
 	b64_ntop((u_char *)signature,
@@ -408,7 +542,7 @@ int send_response(int *sock,
 	response = (char *) calloc(strlen(message) + 1 +
 				   strlen(signature_b64), sizeof(char));
 	if (response == NULL){
-		D("Unable to allocate memory for response");
+		D("Error: Unable to allocate memory for response");
 		return(errno);
 	}
 	strcat(response, message);
@@ -420,7 +554,7 @@ int send_response(int *sock,
 
 	/* Send response */
 	if (send(*sock, response, strlen(response), 0) == -1){
-		D("Error sending response");
+		D("Error: sending response");
 		return(errno);
 	}
 
@@ -469,30 +603,14 @@ int main (int argc, char *argv[])
 		if (!fork()){ // child process
 			close(sock); // child doesn't need the listener
 
-			// seed some bytes into the PRNG
-			FILE *rand_f;
-			int seed;
-			struct timeval tv;
-			if((rand_f = fopen("/dev/random", "r")) == NULL){
-				gettimeofday(&tv,0);
-				seed = tv.tv_sec + tv.tv_usec;
-			} else {
-				if(fread(&seed,sizeof(seed),1,rand_f) == 1) {
-					fclose(rand_f);
-				} else {
-					gettimeofday(&tv,0);
-					seed = tv.tv_sec + tv.tv_usec;
-				}
-			}
-			RAND_seed((const void *)&seed, sizeof(seed));
 
 			/* Send the nonce */
 			char *strnonce;
 			strnonce = (char *) calloc(17, sizeof(char));
-			result = send_nonce(&newsock, &strnonce);
+			result = send_nonce(newsock, &strnonce);
 			if (result != SIBYL_SUCCESS){
 				D("Error sending the nonce");
-				exit(SIBYL_NONCE_ERROR);
+				exit(result);
 			}
 
 			/* Receive the client's message and parse it */
@@ -504,7 +622,7 @@ int main (int argc, char *argv[])
 				exit(errno);
 			}
 			result = receive_msg(&msg,
-					     &newsock,
+					     newsock,
 					     token);
 			if (result != SIBYL_SUCCESS){
 				D("Error receiving the client's message");
@@ -547,7 +665,6 @@ int main (int argc, char *argv[])
 			}
 
 			D1("p2_data: %s\n", p2_data);
-
 
 			/* Is the password correct */
 			char *auth_result = calloc(1, sizeof(char));
