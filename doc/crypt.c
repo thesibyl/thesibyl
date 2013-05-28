96b7281bab356bf332f7175505df165c6cc2d284
Rafael Casado Sanchez <rafacas@gmail.com>
2013-05-28 22:50:44 +0100

    Moved pam-sibyl and testing tools to other repositories

---
+++ doc/crypt.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int main (int argc, char *argv[]){
+
+	if(argc != 2){
+		printf("Usage: %s [password]\n", argv[0]);
+	}
+	
+	printf("%s\n", crypt(argv[1], "$1$326428$"));
+	exit(1);
+
+
+}
