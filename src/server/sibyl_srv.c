8d812f60d0dd3d4ce6abc670497d14e25aaaa165
Pedro Fortuny Ayuso <info@pfortuny.net>
2013-09-25 10:00:58 +0200

    Some swear words have been censored.

--- src/server/sibyl_srv.c
+++ src/server/sibyl_srv.c
@@ -216,14 +216,15 @@ int main (int argc, char *argv[]){
 
                         D1("token[1]:{%s}\n", token[1]);
                         /* this is pathetic */
-                        char *puta = (char *)calloc(1024,1);
-                        memcpy(puta, token[1], strlen(token[1]));
+                        char *resp = (char *)calloc(RSA_size(decrypt) + 1,
+                                                    sizeof(u_char));
+                        memcpy(resp, token[1], strlen(token[1]));
 			retval = decrypt_token(p1_data,
                                                command,
-					       puta,
+					       resp,
 					       decrypt);
 			if (retval != SIBYL_SUCCESS){
-                                printf("Fuck\n");
+                                printf("Decryption error\n");
                                 goto ENDCHILD;
 			}
 
